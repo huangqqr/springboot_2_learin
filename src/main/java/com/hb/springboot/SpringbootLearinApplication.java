bafbafaa1ef210bd5fa73a7de229289502059cc5
huangbo <18255558531@163.com>
2019-08-19 21:35:08 +0800

    helloword

---
+++ src/main/java/com/hb/springboot/SpringbootLearinApplication.java
@@ -0,0 +1,13 @@
+package com.hb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearinApplication.class, args);
+    }
+
+}
